e69a2adff45a51fd3c9380968b03ee29a1fc3f53
JSpeedie <julianspeedie@gmail.com>
2016-01-29 03:50:35 +0000

    Improved restore-setup-arch1.sh, Added fading in .Xresources and changed bash prompt

--- restore-setup-arch1.sh
+++ restore-setup-arch1.sh
@@ -1,115 +1,70 @@
 # Installs everything needed for this setup which is, primarily, bspwm + sxhkd but also all the things I use like
 # urxvt, xrandr, firefox, alsamixer, screenfetch, xinit, etc.
 
-PKGLIST=(bspwm sxhkd conky rofi feh rxvt-unicode xorg-xinit dunst libnotify i3lock)
+PKGLIST=(bspwm sxhkd conky rofi feh rxvt-unicode xorg-xinit dunst libnotify vim i3lock)
 OPKGLIST=(xorg-xrandr firefox alsa-utils scrot screenfetch flashplugin unzip)
+YPKGLIST=(compton lemonbar-xft-git)
 
 cd ~
 clear
-printf "========================================\n"
-printf "=             SETUP SCRIPT             =\n"
-printf "========================================\n"
-printf "Distro: Arch\nShell: bash\nSetup:\n\tWM: bspwm\n\tPanel/Bar: kryptn-bar/lemonbar-xft\n\tLauncher: rofi\n\tFont: Hermit\n"
+echo " ╔════════════════════════════════════════════════╗ "
+echo " ║                  SETUP SCRIPT                  ║ "
+echo " ╚════════════════════════════════════════════════╝ "
+echo "                   Distro: Arch"
+echo "                    Shell: bash"
+echo "                       WM: bspwm"
+echo "                Panel/Bar: lemonbar-xft-git"
+echo "                 Launcher: rofi"
+echo "                     Font: Hermit"
+echo "            Notifications: dunst"
+echo "               Compositor: compton"
 echo
-echo "Packages: (${PKGLIST[*]})"
-printf "Choose packages to install (default=all): "
-read -a LIST
-if [[ ${LIST[*]} == "" ]];
-then
-	printf "Ask for confirmation for each package? [Y/n] "
-	read ANS
-	if [[ $ANS == "Y" ]];
-	then
-		for i in ${PKGLIST[*]};
-		do
-			sudo pacman -S $i
+for i in $(printf "PKGLIST\nOPKGLIST\nYPKGLIST"); do
+	if [[ $i == "PKGLIST" ]]; then
+		CPKGL=("${PKGLIST[@]}")
+	elif [[ $i == "OPKGLIST" ]]; then
+		CPKGL=("${OPKGLIST[@]}")
+	elif [[ $i == "YPKGLIST" ]]; then
+		CPKGL=("${YPKGLIST[@]}")
+	else
+		CPKGL=("${PKGLIST[@]}")
+	fi
+	echo
+	echo "Packages: (${CPKGL[*]})"
+	echo "Type any non-number/whitespace character(s) to exit"
+	printf "Enter n° of packages to be installed (ex: 1 2 3) (enter=all) "
+	read -a INPUT
+	if [[ ${INPUT[*]} == "" ]]; then
+		printf "Ask for confirmation for each package? [Y/n] "
+		read ANS
+		for j in ${CPKGL[*]}; do
+			if [[ $ANS == "Y" ]]; then
+				sudo pacman -S $j
+			elif [[ $ANS == "n" ]]; then
+				sudo pacman -S $j --noconfirm
+			else
+				echo "Incorrect input. Exiting..."
+				exit 1
+			fi
 		done
-	elif [[ $ANS == "n" ]];
-	then
-		for i in ${PKGLIST[*]};
-		do
-			sudo pacman -S $i --noconfirm
+	elif [[ ${INPUT[*]} =~ ^([0-9]{1,}[ \t]*){1,}$ ]]; then
+		printf "Ask for confirmation for each package? [Y/n] "
+		read ANS
+		# manipulate input to get the numbers they wanna install.
+		# for loop through those numbers and install each package
+		for j in ${INPUT[*]}; do
+			if [[ $ANS == "Y" ]];
+			then
+				sudo pacman -S ${CPKGL[j-1]}
+			elif [[ $ANS == "n" ]];	then
+				sudo pacman -S ${CPKGL[j-1]} --noconfirm
+			else
+				echo "Incorrect input. Exiting..."
+				exit 1
+			fi
 		done
 	else
 		echo "Incorrect input. Exiting..."
+		exit 1
 	fi
-elif [[ ${LIST[*]} =~ ^([0-9]{1,}[ \t]*){1,}$ ]];
-then
-	printf "Ask for confirmation for each package? [Y/n] "
-	read ANS
-	if [[ $ANS == "Y" ]];
-		then
-		# manipulate string to get the numbers you wanna install
-		# for loop through those numbers and install each one
-		for i in ${LIST[*]};
-		do
-			sudo pacman -S ${PKGLIST[$i]}
-		done
-	elif [[ $ANS == "n" ]];
-	then
-		# manipulate string to get the numbers you wanna install
-		# for loop through those numbers and install each one
-		for i in ${LIST[*]};
-		do
-			sudo pacman -S ${PKGLIST[$i]} --noconfirm
-		done
-	else
-		echo "Incorrect input. Exiting..."
-	fi
-else
-	echo "Incorrect input. Exiting..."
-fi
-
-#
-# Install optional packages (mostly just for me when I reinstall arch)
-#
-
-echo "Packages: (${OPKGLIST[*]})"
-echo "Enter any non number/digit if you don't want to install the optonal packages (ex. \"n\")"
-printf "Choose optional packages to install (default=all): "
-read -a LIST
-if [[ ${LIST[*]} == "" ]];
-then
-	printf "Ask for confirmation for each package? [Y/n] "
-	read ANS
-	if [[ $ANS == "Y" ]];
-	then
-		for i in ${OPKGLIST[*]};
-		do
-			sudo pacman -S $i
-		done
-	elif [[ $ANS == "n" ]];
-	then
-		for i in ${OPKGLIST[*]};
-		do
-			sudo pacman -S $i --noconfirm
-		done
-	else
-		echo "Incorrect input. Exiting..."
-	fi
-elif [[ ${LIST[*]} =~ ^([0-9]{1,}[ \t]*){1,}$ ]];
-then
-	printf "Ask for confirmation for each package? [Y/n] "
-	read ANS
-	if [[ $ANS == "Y" ]];
-		then
-		# manipulate string to get the numbers you wanna install
-		# for loop through those numbers and install each one
-		for i in ${LIST[*]};
-		do
-			sudo pacman -S ${OPKGLIST[$i]}
-		done
-	elif [[ $ANS == "n" ]];
-	then
-		# manipulate string to get the numbers you wanna install
-		# for loop through those numbers and install each one
-		for i in ${LIST[*]};
-		do
-			sudo pacman -S ${OPKGLIST[$i]} --noconfirm
-		done
-	else
-		echo "Incorrect input. Exiting..."
-	fi
-else
-	echo "Incorrect input. Exiting..."
-fi
+done
