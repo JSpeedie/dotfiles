12ae20f4ac1a52596b81936dd9127971f67a784c
JSpeedie <julianspeedie@gmail.com>
2016-02-08 14:59:22 +0000

    Removed network up and down and replaced with NetUp which just represents whether any network interface is up

--- .lemonbarscripts/lbarout.sh
+++ .lemonbarscripts/lbarout.sh
@@ -6,8 +6,7 @@
 
 # Icons
 IUpTime=""
-IUp=""
-IDown=""
+INet=""
 IMem=""
 ICpuTemp=""
 ICpuLoad=""
@@ -36,12 +35,14 @@ SEP4="    "
 SEP6="      "
 # }}}
 
+# Other stuff
+refresh=0.25
+refreshflip=4
+
 # Create temp files
 mkdir -p /tmp/.lemonbarscripts
 echo "false" >/tmp/.lemonbarscripts/cputnotif
 echo "false" >/tmp/.lemonbarscripts/batterynotif
-echo "0" >/tmp/.lemonbarscripts/netdown
-echo "0" >/tmp/.lemonbarscripts/netup
 
 # Names of all the screen outputs being used
 Screens=$(xrandr | grep -o "^.* connected" | sed "s/ connected//")
@@ -90,8 +91,12 @@ bar() {
 
 	Brightness() {
 		BRIGHTNESS=$(xbacklight -get | grep -o "[0-9]\+\.[0-9]\?")
-		BRIGHTNESS+="%"
-		echo %{F$gray}$IBrightness$SEP$BRIGHTNESS%{F-}
+		if [[ $BRIGHTNESS != "" ]]; then
+			BRIGHTNESS+="%"
+			echo %{F$gray}$IBrightness$SEP$BRIGHTNESS%{F-}
+		else
+			echo ""
+		fi
 	}
 
 	CpuTemp() {
@@ -138,72 +143,15 @@ bar() {
 		echo %{F$gray}$IMem$SEP$MEMUSED%{F-}
 	}
 
-	NetworkDown() {
-		LASTSAMPLE=$(cat /tmp/.lemonbarscripts/netdown)
-		SAMPLE=$(cat /proc/net/dev | grep ".*:[ \t]\+[0-9]\+" | awk '{print $2}')
-		Down=0
-		for bytes in $(echo $SAMPLE); do
-			let Down+=$bytes
-		done
-		let FDown=$Down
-		let FDown=$FDown-$LASTSAMPLE
-		# Conversion info {{{
-		# convert from B to KB = /1000, from /0.05s to /s = *20
-		# We want KB/s then from there we'll chop it up into MB/s. We do that instead of
-		# just converting to MB/s because bash doesn't do floats so we have to simulate
-		# them with strings and string manipulations
-		# /1000 for KB, *20 for /s }}}
-		let FDown/=20
-		FStrO=$(echo $FDown)
-		while [[ $(expr length $FStrO) < 4 ]]; do
-			FStrO="0"$FStrO
-		done
-		# Get the non decimal portion of the MB/s value
-		FStr=$(echo $FStrO | sed "s/[0-9]\{1,3\}$//")
-		FStr+="."
-		# Gets the decimal values for the MB/s value
-		FStr+=$(echo $FStrO | grep -o "[0-9]\{1,3\}$")
-		# Remove the last digit so that the final result is to 2 decimals
-		FStr=$(echo $FStr | sed "s/[0-9]$//")
-		FStr+="MB"
-
-		echo %{F$gray}$IDown$SEP$FStr%{F-}
-		# Save as "LASTSAMPLE"
-		echo $Down >/tmp/.lemonbarscripts/netdown
-	}
-
-	NetworkUp() {
-		LASTSAMPLE=$(cat /tmp/.lemonbarscripts/netup)
-		SAMPLE=$(cat /proc/net/dev | grep ".*:[ \t]\+[0-9]\+" | awk '{print $10}')
-		Up=0
-		for bytes in $(echo $SAMPLE); do
-			let Up+=$bytes
-		done
-		let FUp=$Up
-		let FUp=$FUp-$LASTSAMPLE
-		# Conversion info {{{
-		# convert from B to KB = /1000, from /0.05s to /s = *20
-		# We want KB/s then from there we'll chop it up into MB/s. We do that instead of
-		# just converting to MB/s because bash doesn't do floats so we have to simulate
-		# them with strings and string manipulations
-		# /1000 for KB, *20 for /s }}}
-		let FUp/=20
-		FStrO=$(echo $FUp)
-		while [[ $(expr length $FStrO) < 4 ]]; do
-			FStrO="0"$FStrO
-		done
-		# Get the non decimal portion of the MB/s value
-		FStr=$(echo $FStrO | sed "s/[0-9]\{1,3\}$//")
-		FStr+="."
-		# Gets the decimal values for the MB/s value
-		FStr+=$(echo $FStrO | grep -o "[0-9]\{1,3\}$")
-		# Remove the last digit so that the final result is to 2 decimals
-		FStr=$(echo $FStr | sed "s/[0-9]$//")
-		FStr+="MB"
-
-		echo %{F$gray}$IUp$SEP$FStr%{F-}
-		# Save as "LASTSAMPLE"
-		echo $Up >/tmp/.lemonbarscripts/netup
+	NetUp() {	
+		Down=$(ifstat | grep enp5s0 | awk '{print $6}')
+		NetUp=$(ip a | grep -v "lo" | grep "<" | grep "UP")
+		# If some network interface is up
+		if [[ NetUp != "" ]]; then
+			echo %{F$gray}$INet%{F-}
+		else
+			echo ""
+		fi
 	}
 
 	Time() {
@@ -231,8 +179,8 @@ bar() {
 		
 		# This should maybe be changed so that it doesn't serach for Left but rather just a percent
 		# exec amixer -D pulse get Master | grep Left: | grep -o "[0-9]*%" | grep -o "[0-9]*"
-		# VOL=$(amixer -D pulse get Master | grep Left: | grep -o "[0-9]*%" || echo "--") }}}
-		VOL=50
+		VOL=$(amixer get Master | grep Left: | grep -o "[0-9]*%" || echo "--")
+		# }}}
 		echo %{F$gray}%{A:urxvt -e "alsamixer -V all &":}$IVolS$SEP$VOL%{A}%{F-}
 	}
 
@@ -274,24 +222,28 @@ bar() {
 		echo "$workspace"
 	}
 
-	echo "%{l}$SEP2$(UpTime)$SEP2$(CpuTemp)$SEP2$(Memory)$SEP2$(NetworkUp)$SEP2$(NetworkDown)%{c}$(Workspaces)%{r}$(Brightness)$SEP2$(Battery)$SEP2$(Volume)$SEP2$(Date)$SEP2$(Time)$SEP2"
+	barleft="$SEP2$(UpTime)$SEP2$(CpuTemp)$SEP2$(Memory)$SEP2$(NetUp)"
+	barcenter="$(Workspaces)"
+	barright="$(Brightness)$SEP2$(Battery)$SEP2$(Volume)$SEP2$(Date)$SEP2$(Time)$SEP2"
+	
+	# ="%{S0}%{l}$barleft%{c}$barcenter%{r}$barright
+	# finalbarout=""
+	# tmp=0
+	# for screen in $Screens; do
+		# finalbarout="${finalbarout}%{S${tmp}}%{l}$barleft%{c}$barcenter%{r}$barright"
+		# tmp=$tmp+1
+	# done
+
+	echo "%{S0}%{l}$barleft%{c}$barcenter%{r}$barright%{S1}%{l}$barleft%{c}$barcenter%{r}$barright"
+	# echo $finalbarout
 }
 
-# Make a new bar for each monitor the system has
-for screen in $(echo $Screens); do
-	echo
-	echo "this is the screen      $screen"
-	echo
-	# Get information about the screen like its dimensions
-	BarXY=$(xrandr | grep $screen | grep -o "+[0-9]\++[0-9]\+")
-	ScreenWidth=$(xrandr | grep $screen | grep -o "[0-9]\+x" | sed "s/x//")
-	# Final qualities of the bar. Width and X and Y
-	Dimensions=$(echo "${ScreenWidth}x30${BarXY}")
-	echo
-	echo "this is the bar final dimensions      $Dimensions"
-	echo
-	while true; do
-		echo "$(bar)"
-		sleep 0.05;
-	done | lemonbar -g $Dimensions -a 11 -u 2 -o 1 -f "Hermit-10" -o -1 -f "FontAwesome-11" -B $bg -F $fg | bash &
-done
+# Get information about the screen like its dimensions
+# BarXY=$(xrandr | grep $screen | grep -o "+[0-9]\++[0-9]\+")
+# ScreenWidth=$(xrandr | grep $screen | grep -o "[0-9]\+x" | sed "s/x//")
+# Final qualities of the bar. Width and X and Y
+# Dimensions=$(echo "${ScreenWidth}x30${BarXY}")
+while true; do
+	echo "$(bar)"
+	sleep $refresh;
+done | lemonbar -g x30 -a 22 -u 2 -o 1 -f "Hermit-10" -o -1 -f "FontAwesome-11" -B $bg -F $fg | bash &
