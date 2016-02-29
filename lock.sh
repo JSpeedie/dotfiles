3dfe0b709034b79bf0b0f3a46d0491bdcea17fde
JSpeedie <julianspeedie@gmail.com>
2016-02-29 02:44:03 -0500

    Removed duplicates of relocated sh files in home folder

--- lock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ICON=$HOME/LockIcon.png
-BG=/tmp/screen.png
-scrot /tmp/screen.png
-convert $BG -scale 10% -scale 1000% $BG
-convert $BG $ICON -gravity center -composite -matte $BG
-i3lock -u -i $BG
