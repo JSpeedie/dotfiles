2c1e905120da5b8177ec7219739ee4fde09d54a6
JSpeedie <julianspeedie@gmail.com>
2016-02-29 03:57:13 -0500

    Updated .bashrc and .xinitrc so they now work with the relocated sh files. updatedotgit.sh now updates files based on what the git is tracking rather than a user specified list

--- scripts/updatedotgit.sh
+++ scripts/updatedotgit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+First=(~/)
+Second=(~/dotfilesGit/)
+
 red=$'\e[1;31m'
 green=$'\e[1;32m'
 yellow=$'\e[1;33m'
@@ -9,10 +12,10 @@ cyan=$'\e[1;36m'
 end=$'\e[0m'
 
 # Files that need to be updated
-FLIST=(.config/bspwm/bspwmrc .config/sxhkd/sxhkdrc .lemonbarscripts/lbarout.sh .Xresources .bash_profile .bashrc .gtkrc-2.0 .profile .vimrc .xinitrc compton.conf dunstrc restore-setup-arch1.sh install.sh updatedotgit.sh .vim/colors/gruvbox.vim .vim/colors/ryuuko.vim Pictures/Wallpapers/solid.jpg Pictures/Wallpapers/LifeGuardPostAtBeach.jpe coloursconf)
-
-First=(~/)
-Second=(~/dotfilesGit/)
+# FLIST=(.config/bspwm/bspwmrc .config/sxhkd/sxhkdrc .lemonbarscripts/lbarout.sh .Xresources .bash_profile .bashrc .gtkrc-2.0\
+# .profile .vimrc .xinitrc compton.conf dunstrc restore-setup-arch1.sh install.sh updatedotgit.sh .vim/colors/gruvbox.vim\
+# .vim/colors/ryuuko.vim Pictures/Wallpapers/solid.jpg Pictures/Wallpapers/LifeGuardPostAtBeach.jpe coloursconf)
+FLIST=($(cd $Second; git ls-tree -r master --name-only | grep -v "\(hermit\)\|\(font-awesome\)\|\(README.md\)")) 
 
 printf "File path for first spot (default=$First) "
 read FirstRead
@@ -32,15 +35,22 @@ if [[ $LISTIF != "Y" ]] && [[ $LISTIF != "n" ]] && [[ $LISTIF != "" ]]; then
 	exit 1
 fi
 
-for file in ${FLIST[*]}; do
-	exitstatus=$(cmp -s $First$file $Second$file; echo $?)
+for file in ${FLIST[@]}; do
+	if [[ -d $First$file ]]; then
+		continue
+	fi
+
+	difftest=$(cmp -s $First$file $Second$file; echo $?)
 	if [[ $LISTIF == "Y" ]] || [[ $LISTIF == "" ]]; then
 		# If the files aren't different, continue.
-		if [[ $exitstatus == "0" ]]; then continue; fi
+		if [[ $difftest == "0" ]]; then continue; fi
 	fi
 	# Print if the files differ or not
-	if [[ $exitstatus != "0" ]]; then echo "$red==> The files differ$end";
-	else echo "$green==> The files do NOT differ$end"; fi
+	if [[ $difftest != "0" ]]; then
+		echo "$red==> The files differ$end"
+	else
+		echo "$green==> The files do NOT differ$end"
+	fi
 
 	echo "Y=Yes, n=no, c=compare"
 	printf "Update $file? [Y/n/c] "
