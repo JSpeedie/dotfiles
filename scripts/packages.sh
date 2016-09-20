b1dcde8c996a9bcc2ba59ece32e92f2ddfdba0fc
JSpeedie <julianspeedie@gmail.com>
2016-09-20 15:30:58 -0400

    Added ttf-dejavu to fix code sections of websites rendering as unicode characters

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -11,7 +11,7 @@ cyan=$'\e[1;36m'
 end=$'\e[0m'
 
 PKGLIST=(bspwm sxhkd lm_sensors rofi feh rxvt-unicode xorg xorg-xinit \
-	xorg-xrandr dunst libnotify pulseaudio pamixer mpd mpc ctags)
+	xorg-xrandr dunst libnotify pulseaudio pamixer mpd mpc ctags ttf-dejavu)
 OPKGLIST=(firefox alsa-utils scrot screenfetch flashplugin unzip zip vim eog \
 	gimp nautilus xorg-xfontsel ntfs-3g pandoc texlive-core mtp gvfs-mtp)
 YPKGLIST=(compton lemonbar-xft-git)
