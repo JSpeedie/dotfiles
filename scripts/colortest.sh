b90f028ca57f073574d706a3230485138cd41851
JSpeedie <julianspeedie@gmail.com>
2017-12-02 12:00:39 -0500

    colortest.sh now can take 2 arguments to modify width and height of blocks

--- scripts/colortest.sh
+++ scripts/colortest.sh
@@ -16,22 +16,81 @@ color12=$'\e[1;34m'
 color13=$'\e[1;35m'
 color14=$'\e[1;36m'
 color15=$'\e[1;37m'
+block='█'
+# If there is no first argument
+if [[ -z $1 ]]; then
+	printf "$color0████ "
+	printf "$color1████ "
+	printf "$color2████ "
+	printf "$color3████ "
+	printf "$color4████ "
+	printf "$color5████ "
+	printf "$color6████ "
+	printf "$color7████ "
+	printf "$end\n"
+	printf "$color08████ "
+	printf "$color09████ "
+	printf "$color10████ "
+	printf "$color11████ "
+	printf "$color12████ "
+	printf "$color13████ "
+	printf "$color14████ "
+	printf "$color15████ "
+	printf "$end\n"
+# If a command line argument is given, print that many rows for each colour
+else
+	# If there is a first argument but no second argument
+	if [[ -z $2 ]]; then
+		for i in $(seq $1); do
+			printf "$color0████ "
+			printf "$color1████ "
+			printf "$color2████ "
+			printf "$color3████ "
+			printf "$color4████ "
+			printf "$color5████ "
+			printf "$color6████ "
+			printf "$color7████ "
+			printf "$end\n"
+		done
+		for i in $(seq $1); do
+			printf "$color08████ "
+			printf "$color09████ "
+			printf "$color10████ "
+			printf "$color11████ "
+			printf "$color12████ "
+			printf "$color13████ "
+			printf "$color14████ "
+			printf "$color15████ "
+			printf "$end\n"
+		done
+	# If there is a first and second argument
+	else
+		block_out=""
+		for i in $(seq $1); do
+			block_out=$(echo "${block_out}${block}")
+		done
+		for i in $(seq $2); do
+			printf "$color0${block_out} "
+			printf "$color1${block_out} "
+			printf "$color2${block_out} "
+			printf "$color3${block_out} "
+			printf "$color4${block_out} "
+			printf "$color5${block_out} "
+			printf "$color6${block_out} "
+			printf "$color7${block_out} "
+			printf "$end\n"
+		done
+		for i in $(seq $2); do
+			printf "$color08${block_out} "
+			printf "$color09${block_out} "
+			printf "$color10${block_out} "
+			printf "$color11${block_out} "
+			printf "$color12${block_out} "
+			printf "$color13${block_out} "
+			printf "$color14${block_out} "
+			printf "$color15${block_out} "
+			printf "$end\n"
+		done
+	fi
+fi
 
-printf "$color0████ "
-printf "$color1████ "
-printf "$color2████ "
-printf "$color3████ "
-printf "$color4████ "
-printf "$color5████ "
-printf "$color6████ "
-printf "$color7████ "
-printf "$end\n"
-printf "$color08████ "
-printf "$color09████ "
-printf "$color10████ "
-printf "$color11████ "
-printf "$color12████ "
-printf "$color13████ "
-printf "$color14████ "
-printf "$color15████ "
-printf "$end\n"
