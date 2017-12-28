dee64e57e2fd26293cafe7fdfad35619903511de
JSpeedie <julianspeedie@gmail.com>
2017-12-28 15:07:50 -0500

    Changed source for bonsai to coloured output version

--- scripts/bonsai.sh
+++ scripts/bonsai.sh
@@ -1,19 +1,21 @@
+#!/bin/bash
 # Prints a color text image of a bonsai tree
-# Credits to Github user 'findlang'
-# Found here: https://gist.github.com/findlang/5da0ef0bddf087e8d700d1563cea41d8
 
-echo -e "              &&"
-echo -e "             &&&&&"
-echo -e "           &&&\/& &&&"
-echo -e "          &&|,/  |/& &&"
-echo -e "           &&/   /  /_&  &&"
-echo -e "             \  {  |_____/_&"
-echo -e "             {  / /          &&&"
-echo -e "             .\`. \\{___\________\/_\}"
-echo -e "              \} \}\{       \\"
-echo -e "              }\{\{         \\____&"
-echo -e "             \{\}\{           \`&\&&"
-echo -e "             {{}             &&"
-echo -e "       , -=-~{ .-^- _"
+# Credits to reddit user 'GuinansEyebrows' who provided the link
+# 'https://x.eti.tf/tpG7d.sh'
+
+echo -e "              \e[32m&&"
+echo -e "             \e[32m&&&&&"
+echo -e "           \e[32m&&&\/& &&&"
+echo -e "          \e[32m&&\e[39m|,/  |/\e[32m& &&"
+echo -e "           \e[32m&&\e[39m/   /  /_\e[32m&  &&"
+echo -e "             \e[39m\  {  |_____/_\e[32m&"
+echo -e "             \e[39m{  / /          \e[32m&&&"
+echo -e "             \e[39m.\`. \\{___\________\/_\}"
+echo -e "              \e[39m\} \}\{       \\"
+echo -e "              \e[39m}\{\{         \\____\e[32m&"
+echo -e "             \e[39m\{\}\{           \`\e[32m&\e[39m\\e[32m&&"
+echo -e "             \e[39m{{}             \e[32m&&"
+echo -e "       \e[39m, -=-~{ .-^- _"
 echo -e "             \`}"
 echo -e "              {"
