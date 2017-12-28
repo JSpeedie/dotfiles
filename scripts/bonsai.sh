115aac50832602aecf7422ea0daf248fed8e01b0
JSpeedie <julianspeedie@gmail.com>
2017-12-28 15:04:24 -0500

    Added bonsai script for all necessary aesthetic memes and needs

---
+++ scripts/bonsai.sh
@@ -0,0 +1,19 @@
+# Prints a color text image of a bonsai tree
+# Credits to Github user 'findlang'
+# Found here: https://gist.github.com/findlang/5da0ef0bddf087e8d700d1563cea41d8
+
+echo -e "              &&"
+echo -e "             &&&&&"
+echo -e "           &&&\/& &&&"
+echo -e "          &&|,/  |/& &&"
+echo -e "           &&/   /  /_&  &&"
+echo -e "             \  {  |_____/_&"
+echo -e "             {  / /          &&&"
+echo -e "             .\`. \\{___\________\/_\}"
+echo -e "              \} \}\{       \\"
+echo -e "              }\{\{         \\____&"
+echo -e "             \{\}\{           \`&\&&"
+echo -e "             {{}             &&"
+echo -e "       , -=-~{ .-^- _"
+echo -e "             \`}"
+echo -e "              {"
