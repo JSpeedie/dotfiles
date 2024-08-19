dde3d42cd2e340cd7f12ca5f0340a4bd0745b586
JSpeedie <julianspeedie@gmail.com>
2024-08-19 18:11:49 -0400

    Switched neovim setup to use lazy.nvim

--- .config/nvim/after/plugin/nvim-cmp.lua
+++ .config/nvim/after/plugin/nvim-cmp.lua
@@ -1,7 +1,40 @@
 local cmp = require'cmp'
 local lspkind = require('lspkind')
 
+local kind_icons = {
+  Text = "",
+  Method = "󰆧",
+  Function = "󰊕",
+  Constructor = "",
+  Field = "󰇽",
+  Variable = "󰂡",
+  Class = "󰠱",
+  Interface = "",
+  Module = "",
+  Property = "󰜢",
+  Unit = "",
+  Value = "󰎠",
+  Enum = "",
+  Keyword = "󰌋",
+  Snippet = "",
+  Color = "󰏘",
+  File = "󰈙",
+  Reference = "",
+  Folder = "󰉋",
+  EnumMember = "",
+  Constant = "󰏿",
+  Struct = "",
+  Event = "",
+  Operator = "󰆕",
+  TypeParameter = "󰅲",
+}
+
 cmp.setup({
+  -- This completion section that disables autocompletion is important for adding
+  -- a delay to when the autocompletion shows up.
+  completion = {
+    autocomplete = false
+  },
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
@@ -15,9 +48,9 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    -- ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
@@ -25,24 +58,45 @@ cmp.setup({
   }, {
     { name = 'buffer' },
   }),
+  -- This 'formatting' section is necessary to get icons next to the type of
+  -- the autocompletion recommendation
   formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol', -- show only symbol annotations
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-                     -- can also be a function to dynamically calculate max width such as 
-                     -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      before = function (entry, vim_item)
-        ...
-        return vim_item
-      end
-    })
+    format = lspkind.cmp_format(),
   },
 })
+
+-- Add a delay so that the auto complete only shows up after a 750ms pause
+-- {{{
+local completionDelay = 750
+local timer = nil
+
+function _G.setAutoCompleteDelay(delay)
+  completionDelay = delay
+end
+
+function _G.getAutoCompleteDelay()
+  return completionDelay
+end
+
+vim.api.nvim_create_autocmd({ "TextChangedI", "CmdlineChanged" }, {
+  pattern = "*",
+  callback = function()
+    if timer then
+      vim.loop.timer_stop(timer)
+      timer = nil
+    end
+
+    timer = vim.loop.new_timer()
+    timer:start(
+      _G.getAutoCompleteDelay(),
+      0,
+      vim.schedule_wrap(function()
+        cmp.complete({ reason = cmp.ContextReason.Auto })
+      end)
+    )
+  end,
+})
+-- }}}
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ '/', '?' }, {
