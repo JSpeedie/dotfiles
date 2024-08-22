7040f084598a0a9ea8a06c8b3bfc52bac7dbb435
JSpeedie <julianspeedie@gmail.com>
2024-08-21 22:24:59 -0400

    Switched to lualine - vim now boots in almost half the time

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -1,1 +1,4 @@
--- (Currently empty)
+-- Add key bindings for going to the next diagnostic. Useful for moving to
+-- the next error or warning
+vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
+vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
