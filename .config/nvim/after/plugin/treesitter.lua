161ae283a7894687f5676b80d0ac498853f9d70d
JSpeedie <julianspeedie@gmail.com>
2024-08-24 19:45:11 -0400

    Added LSP support for vimscript and Lua

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,8 +1,9 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
   -- https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
-  ensure_installed = { "c", "cpp", "rust", "python", "lua", "html", "javascript", "markdown", "markdown_inline" },
-  -- ensure_installed = { "c", "cpp", "rust", "python", "lua", "html", "javascript", "latex", "markdown", "markdown_inline" },
+  ensure_installed = { "c", "cpp", "rust", "python", "lua", "html",
+    "javascript", "markdown", "markdown_inline", "vim" },
+  -- ensure_installed = { "latex", },
   -- latex requires tree-sitter-cli, which can be installed using 'npm install -g tree-sitter-cli'
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
